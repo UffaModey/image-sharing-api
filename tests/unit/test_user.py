a2964f8ad5bbbd278808ae2b143da7cc3475099d
Fafa Modey <uffamodey@gmail.com>
2024-10-15 17:33:40 +0100

    unit tests for posts, likes and follow features

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -2,31 +2,13 @@ import logging
 import pytest
 
 from tests import factories as f
+from tests.utils import _test_authenticate_user
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
 pytestmark = pytest.mark.django_db
-
-
-def _test_authenticate_user(api_client, username, password) -> dict:
-    """
-    Helper function to authenticate a user and return an access token.
-    """
-    user = f.create_user(username=username, email='user@foo.com', password=password)
-
-    payload = {
-        "username": username,
-        "password": password
-    }
-    response_create = api_client.post("/api/token/", data=payload)
-    assert response_create.status_code == 200, "Authentication failed"
-
-    # Set the Authorization header with the token
-    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {response_create.data["access"]}')
-    return {"user_id": user.id,
-            "username": user.username}
 
 
 def test_create_user(api_client) -> None:
@@ -42,15 +24,18 @@ def test_create_user(api_client) -> None:
     }
 
     # test create a user
-    response_create = api_client.post("/user/", data=user_data)
-    user_id = response_create.data["id"]
+    response = api_client.post("/user/", data=user_data)
+    user_id = response.data["id"]
     logger.info(f"Created a user with id: {user_id}")
-    logger.info(f"Response: {response_create.data}")
-    assert response_create.status_code == 201
-    assert response_create.data["username"] == user_data["username"]
+    logger.info(f"Response: {response.data}")
+    assert response.status_code == 201
+    assert response.data["username"] == user_data["username"]
 
 
 def test_create_user_with_existing_username(api_client) -> None:
+    """
+    Test create a user with an existing username
+    """
     f.create_user(username="username", email='user@foo.com', password="password")
     user_data = {
         "username": "username",
@@ -58,12 +43,14 @@ def test_create_user_with_existing_username(api_client) -> None:
         "email": "newuser@example.com"
     }
 
-    # test create a user
-    response_create = api_client.post("/user/", data=user_data)
-    assert response_create.status_code == 400
+    response = api_client.post("/user/", data=user_data)
+    assert response.status_code == 400
 
 
 def test_create_user_with_existing_email(api_client) -> None:
+    """
+    Test create a user with an existing email
+    """
     f.create_user(username="username1", email='newuser@example.com', password="password")
     user_data = {
         "username": "username",
@@ -71,31 +58,30 @@ def test_create_user_with_existing_email(api_client) -> None:
         "email": "newuser@example.com"
     }
 
-    # test create a user
-    response_create = api_client.post("/user/", data=user_data)
-    assert response_create.status_code == 400
+    response = api_client.post("/user/", data=user_data)
+    assert response.status_code == 400
 
 
 def test_get_user_profile(api_client) -> None:
-
-    # Authenticate the user and get the access token
+    """
+    Test get a user's profile
+    """
     user = _test_authenticate_user(api_client, 'username', 'password123')
 
-    # Test get a user's profile
-    response_read = api_client.get(f"/user/{user['user_id']}", format="json")
-    assert response_read.status_code == 200, "Failed to retrieve user profile"
-    logger.info(f"Read user with id: {user['user_id']}")
-    logger.info(f"Response: {response_read.data}")
-    assert response_read.data["username"] == user['username']
+    response = api_client.get(f"/user/{user.id}", format="json")
+    assert response.status_code == 200, "Failed to retrieve user profile"
+    logger.info(f"Read user with id: {user.id}")
+    logger.info(f"Response: {response.data}")
+    assert response.data["username"] == user.username
 
 
 def test_list_users(api_client) -> None:
-    """Test listing all users"""
-    # Authenticate the user and get the access token
+    """
+    Test listing all users
+    """
     _test_authenticate_user(api_client, 'username', 'password123')
     f.create_user()
 
-    # Test list users
-    response_read = api_client.get(f"/user/", format="json")
-    assert response_read.status_code == 200, "Failed to list users"
-    assert response_read.data["count"] == 2
+    response = api_client.get(f"/user/", format="json")
+    assert response.status_code == 200, "Failed to list users"
+    assert response.data["count"] == 2
